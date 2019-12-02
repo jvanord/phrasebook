28ddb1f92b4188dc76a2e4cb1791dd236f81427f
Jay VanOrd <jvanord@indasysllc.com>
2019-12-02 12:15:33 -0500

    data presented - no formatting or functionality

--- data.js
+++ data.js
@@ -1,2 +1,752 @@
-var DATA = null;
+var DATA = [
+	{
+	  "category": "Basic",
+	  "english": "Hello / Hi",
+	  "dutch": "Goededag / Hallo / Hoi",
+	  "french": "Bonjour / Salut",
+	  "italian": "Buongiorno / Ciao / Salve"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Good Morning",
+	  "dutch": "Goedemorgen",
+	  "french": "Bonjour",
+	  "italian": "Buongiorno"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Good Afternoon",
+	  "dutch": "Goedemiddag",
+	  "french": "Bonne apr�s-midi",
+	  "italian": "Buon pomeriggio"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Good Evening",
+	  "dutch": "Goedenavond",
+	  "french": "Bonsoir",
+	  "italian": "buona serata"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Good Night",
+	  "dutch": "Goedenacht",
+	  "french": "Bonne nuit",
+	  "italian": "Buona notte"
+	},
+	{
+	  "category": "Basic",
+	  "english": "How are you?",
+	  "dutch": "Hoe gaat het (met u)?",
+	  "french": "Comment allez-vous? / Comment �a va?",
+	  "italian": "Come sta/stai?"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Good / Fine",
+	  "dutch": "Goed",
+	  "french": "Bien / �a va",
+	  "italian": "(Molto) Bene"
+	},
+	{
+	  "category": "Basic",
+	  "english": "See you Later",
+	  "dutch": "Tot ziens",
+	  "french": "� toute � l'heure!",
+	  "italian": "Ci Vediamo"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Goodbye",
+	  "dutch": "Vaarwel / Dag / Hoi",
+	  "french": "Au revoir / � bient�t",
+	  "italian": "Arrivederci / Ciao"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Please",
+	  "dutch": "Alstublieft / Alsjeblieft",
+	  "french": "S'il vous pla�t",
+	  "italian": "per favore"
+	},
+	{
+	  "category": "Basic",
+	  "english": "You're welcome",
+	  "dutch": "Graag gedaan",
+	  "french": "De rien",
+	  "italian": "Prego"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Thank You Very Much",
+	  "dutch": "Dank u (je) Wel / Bedankt",
+	  "french": "Merci beaucoup",
+	  "italian": "Grazie mille"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Yes",
+	  "dutch": "Ja",
+	  "french": "Oui",
+	  "italian": "s�"
+	},
+	{
+	  "category": "Basic",
+	  "english": "No",
+	  "dutch": "Nee",
+	  "french": "Non",
+	  "italian": "No"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Maybe",
+	  "dutch": "Misschien",
+	  "french": "Peut-�tre",
+	  "italian": "Forse"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Excuse Me",
+	  "dutch": "Pardon / Sorry",
+	  "french": "Pardon / Excusez-moi",
+	  "italian": "Scusami"
+	},
+	{
+	  "category": "Basic",
+	  "english": "I'm Sorry",
+	  "dutch": "Sorry / Het spijt me",
+	  "french": "Je suis d�sol�",
+	  "italian": "Mi dispiace"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Sir / Ma'am / Miss",
+	  "dutch": "Meneer / Mevrouw",
+	  "french": "Monsieur / Madame / Madamoiselle",
+	  "italian": "Signore / Signora / Signorina"
+	},
+	{
+	  "category": "Basic",
+	  "english": "Merry Christmas",
+	  "dutch": "Prettige kerstdagen / vrolijk kerstfeest",
+	  "french": "joyeux No�l",
+	  "italian": "Buon Natale"
+	},
+	{
+	  "category": "Help",
+	  "english": "Do you speak English?",
+	  "dutch": "Spreekt u Engels?",
+	  "french": "Parlez vous anglais?",
+	  "italian": "Parli inglese?"
+	},
+	{
+	  "category": "Help",
+	  "english": "Can you please help me?",
+	  "dutch": "Kunt u mij helpen?",
+	  "french": "Pouvez-vous m'aider s'il vous pla�t?",
+	  "italian": "Potete per favore aiutarmi?"
+	},
+	{
+	  "category": "Help",
+	  "english": "Where is the bathroom?",
+	  "dutch": "Waar is het toilet?",
+	  "french": "O� sont les toilettes?",
+	  "italian": "Dov'� il bagno?"
+	},
+	{
+	  "category": "Help",
+	  "english": "I don't know",
+	  "dutch": "Ik weet het niet",
+	  "french": "Je ne sais pas",
+	  "italian": "Non lo so"
+	},
+	{
+	  "category": "Help",
+	  "english": "I don't understand",
+	  "dutch": "Ik begrijp het niet",
+	  "french": "Je ne comprend pas",
+	  "italian": "Non capisco"
+	},
+	{
+	  "category": "Help",
+	  "english": "More slowly please",
+	  "dutch": "Langzamer praten alstublieft",
+	  "french": "Plus lentement si'l vous plait",
+	  "italian": "Pi� lentamente per favore"
+	},
+	{
+	  "category": "Help",
+	  "english": "Please repeat that",
+	  "dutch": "Herall dat alstublieft",
+	  "french": "S'il vous plait, r�p�tez cela",
+	  "italian": "Per favore Ripeti"
+	},
+	{
+	  "category": "Help",
+	  "english": "We / I do not speak <this language>",
+	  "dutch": "Wij spreken geen / Ik spreek geen Nederlands",
+	  "french": "Nous/Je ne parlons/parle pas fran�ais",
+	  "italian": "Non parliamo/parlo italiano"
+	},
+	{
+	  "category": "Help",
+	  "english": "Only a little",
+	  "dutch": "Alleen maar een beetje",
+	  "french": "Juste un peu",
+	  "italian": "Solo un po"
+	},
+	{
+	  "category": "Emergency",
+	  "english": "Help!",
+	  "dutch": "Help!",
+	  "french": "Au secours! / Aidez-moi!",
+	  "italian": "Aiuto!"
+	},
+	{
+	  "category": "Emergency",
+	  "english": "Police",
+	  "dutch": "Politie",
+	  "french": "Police",
+	  "italian": "Polizia"
+	},
+	{
+	  "category": "Emergency",
+	  "english": "Doctor",
+	  "dutch": "Dokter",
+	  "french": "M�decin",
+	  "italian": "Medico"
+	},
+	{
+	  "category": "Emergency",
+	  "english": "Thief",
+	  "dutch": "Dief",
+	  "french": "Voleur",
+	  "italian": "Ladro"
+	},
+	{
+	  "category": "Emergency",
+	  "english": "Hospital",
+	  "dutch": "Ziekenhuis",
+	  "french": "H�pital",
+	  "italian": "Ospedale"
+	},
+	{
+	  "category": "Family",
+	  "english": "We are Americans",
+	  "dutch": "Wij zijn Amerikanen",
+	  "french": "Nous sommes Am�ricains",
+	  "italian": "Siamo americani"
+	},
+	{
+	  "category": "Family",
+	  "english": "We are from the US / Florida",
+	  "dutch": "Wij komen uit de Verenigde Staten",
+	  "french": "Nous venon des �tats Unis / Floride",
+	  "italian": "Siamo degli Stati Uniti / Florida"
+	},
+	{
+	  "category": "Family",
+	  "english": "This is my wife and her parents",
+	  "dutch": "Dit zijn mijn vrouw en haar ouders",
+	  "french": "Ceci est ma femme et ses parents",
+	  "italian": "Questa � mia moglie e i suoi genitori"
+	},
+	{
+	  "category": "Family",
+	  "english": "My wife",
+	  "dutch": "Mijn vrouw",
+	  "french": "Ma femme",
+	  "italian": "Mia moglie"
+	},
+	{
+	  "category": "Family",
+	  "english": "Her husband",
+	  "dutch": "Haar man",
+	  "french": "Son mari",
+	  "italian": "Suo marito"
+	},
+	{
+	  "category": "Family",
+	  "english": "Her mother",
+	  "dutch": "Haar moeder",
+	  "french": "Sa m�re",
+	  "italian": "Sua madre"
+	},
+	{
+	  "category": "Family",
+	  "english": "Her father",
+	  "dutch": "Haar vader",
+	  "french": "Son p�re",
+	  "italian": "Suo padre"
+	},
+	{
+	  "category": "Family",
+	  "english": "From Vietnam",
+	  "dutch": "Uit Vietname",
+	  "french": "Du Vi�t Nam",
+	  "italian": "Dal Vietnam"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "Two coffees please",
+	  "dutch": "Vier koffie alstublieft",
+	  "french": "Deux caf�s s'il vous pla�t",
+	  "italian": "Due caff� per favore"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "I would like this / that",
+	  "dutch": "Ik wil graag dit / dat",
+	  "french": "Je voudrais ceci / cela",
+	  "italian": "Vorrei questo / quello"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "How much is this / that?",
+	  "dutch": "Wat kost dit / dat?",
+	  "french": "Combien �a co�te / est-ce?",
+	  "italian": "Quanto costa questo / quello?"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "Four people",
+	  "dutch": "Vier mensen",
+	  "french": "Quatre personnes",
+	  "italian": "Quattro persone"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "The menu please",
+	  "dutch": "Het menu alstublieft",
+	  "french": "Le menu s'il vous plait",
+	  "italian": "Il menu per favore"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "Some water (natural)",
+	  "dutch": "Een water (natuurlijk)",
+	  "french": "Un peu d'eau (natuelle)",
+	  "italian": "Un po 'd'acqua (naturale)"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "House wine",
+	  "dutch": "Huiswijn",
+	  "french": "Vin de la maison",
+	  "italian": "Di vino della casa"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "What do you recommend?",
+	  "dutch": "Wat raden jullie aan?",
+	  "french": "Que recommandez-vous?",
+	  "italian": "Cosa ci consiglia?"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "Cheers",
+	  "dutch": "Proost",
+	  "french": "� votre sant�",
+	  "italian": "Salute/Saluti/Cin Cin"
+	},
+	{
+	  "category": "Restaurant",
+	  "english": "Check please",
+	  "dutch": "De�rekening�graag",
+	  "french": "L'addition s'il vous plait",
+	  "italian": "Il conto, per favore"
+	},
+	{
+	  "category": "Time",
+	  "english": "Today",
+	  "dutch": "Vandaag",
+	  "french": "Aujourd'hui",
+	  "italian": "Oggi"
+	},
+	{
+	  "category": "Time",
+	  "english": "Tomorrow",
+	  "dutch": "Morgen",
+	  "french": "Demain",
+	  "italian": "Domani"
+	},
+	{
+	  "category": "Time",
+	  "english": "Yesterday",
+	  "dutch": "Gisteren",
+	  "french": "Hier",
+	  "italian": "Ieri"
+	},
+	{
+	  "category": "Time",
+	  "english": "Next Week",
+	  "dutch": "Volgende week",
+	  "french": "Prochaine semaine",
+	  "italian": "La prossima settimana"
+	},
+	{
+	  "category": "Time",
+	  "english": "In the Morning",
+	  "dutch": "(Uur's) Ochtend",
+	  "french": "Du Matin",
+	  "italian": "Di Mattina"
+	},
+	{
+	  "category": "Time",
+	  "english": "It's Noon",
+	  "dutch": "Het is Middag",
+	  "french": "C'est midi",
+	  "italian": "E Mezzogiorno"
+	},
+	{
+	  "category": "Time",
+	  "english": "In the Afternoon",
+	  "dutch": "De middag",
+	  "french": "Dans l'apres midi",
+	  "italian": "Del Pomeriggio"
+	},
+	{
+	  "category": "Time",
+	  "english": "In the Evening",
+	  "dutch": "De Avond",
+	  "french": "Du soir",
+	  "italian": "Di Sera"
+	},
+	{
+	  "category": "Time",
+	  "english": "At Night",
+	  "dutch": "(Uur's) Nacht",
+	  "french": "La Nuit",
+	  "italian": "Di Notte"
+	},
+	{
+	  "category": "Time",
+	  "english": "What time is it?",
+	  "dutch": "Hoe laat is het?",
+	  "french": "Quelle heure est-il?",
+	  "italian": "Che ore sono?"
+	},
+	{
+	  "category": "Time",
+	  "english": "1/2/3 O'Clock",
+	  "dutch": "Een/twee/drie uur",
+	  "french": "Il est une/deux/trois heure",
+	  "italian": "E l'una / Sono le due/tre"
+	},
+	{
+	  "category": "Time",
+	  "english": "1:30 / 3:30",
+	  "dutch": "Half twee / half drie",
+	  "french": "Deux/trois heures et demie",
+	  "italian": "Sono le due/tre e un quarto"
+	},
+	{
+	  "category": "Time",
+	  "english": "3:15 / 4:15",
+	  "dutch": "Kwart over Drie / Kwart over Vier",
+	  "french": "Trois/Quatre heures et quart",
+	  "italian": "Sono le tre/quattro e mezzo"
+	},
+	{
+	  "category": "Time",
+	  "english": "5:45 / 6:45",
+	  "dutch": "Kwart voor vijf / Kwart voor",
+	  "french": "Cinq/Six heures moins le quart",
+	  "italian": "Sono le cinque/sei meno quarto"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 0,
+	  "dutch": "nul",
+	  "french": "zero",
+	  "italian": "zero"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 1,
+	  "dutch": "E�n",
+	  "french": "un",
+	  "italian": "uno"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 2,
+	  "dutch": "twee",
+	  "french": "deux",
+	  "italian": "due"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 3,
+	  "dutch": "drie",
+	  "french": "trois",
+	  "italian": "tre"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 4,
+	  "dutch": "vier",
+	  "french": "quatre",
+	  "italian": "quattro"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 5,
+	  "dutch": "vijf",
+	  "french": "cinq",
+	  "italian": "cinque"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 6,
+	  "dutch": "zes",
+	  "french": "six",
+	  "italian": "cei"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 7,
+	  "dutch": "zeven",
+	  "french": "sept",
+	  "italian": "sette"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 8,
+	  "dutch": "acht",
+	  "french": "huit",
+	  "italian": "otto"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 9,
+	  "dutch": "negen",
+	  "french": "neuf",
+	  "italian": "nove"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 10,
+	  "dutch": "tien",
+	  "french": "dix",
+	  "italian": "dieci"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 11,
+	  "dutch": "elf",
+	  "french": "onze",
+	  "italian": "undici"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 12,
+	  "dutch": "twaalf",
+	  "french": "douze",
+	  "italian": "dodici"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 13,
+	  "dutch": "dertien",
+	  "french": "treize",
+	  "italian": "tredici"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 14,
+	  "dutch": "veertien",
+	  "french": "quatorze",
+	  "italian": "quattordici"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 15,
+	  "dutch": "vijftien",
+	  "french": "quinze",
+	  "italian": "quindici"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 16,
+	  "dutch": "zestien",
+	  "french": "seize",
+	  "italian": "dedici"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 17,
+	  "dutch": "zeventien",
+	  "french": "dix-sept",
+	  "italian": "diciassette"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 18,
+	  "dutch": "achttien",
+	  "french": "dix-huit",
+	  "italian": "diciotto"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 19,
+	  "dutch": "negentien",
+	  "french": "dix-neuf",
+	  "italian": "diciannove"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 20,
+	  "dutch": "twintig",
+	  "french": "vingt",
+	  "italian": "venti"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 21,
+	  "dutch": "eenentwintig",
+	  "french": "vingt-et-un",
+	  "italian": "ventuno"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 22,
+	  "dutch": "twee�ntwintig",
+	  "french": "vingt-deux",
+	  "italian": "ventidue"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 23,
+	  "dutch": "drie�ntwintig",
+	  "french": "vingt-trois",
+	  "italian": "ventitre"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 24,
+	  "dutch": "vierentwintig",
+	  "french": "vingt-quatre",
+	  "italian": "ventiquattro"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 25,
+	  "dutch": "vijfentwintig",
+	  "french": "vingt-cinq",
+	  "italian": "venticinque"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 30,
+	  "dutch": "dertig",
+	  "french": "trente",
+	  "italian": "trenta"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 40,
+	  "dutch": "veertig",
+	  "french": "quarante",
+	  "italian": "quaranta"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 50,
+	  "dutch": "vijftig",
+	  "french": "cinquant",
+	  "italian": "cinquanta"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 60,
+	  "dutch": "zestig",
+	  "french": "soixante",
+	  "italian": "sessanta"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 70,
+	  "dutch": "zeventig",
+	  "french": "soixante-dix",
+	  "italian": "settanta"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 71,
+	  "dutch": "zeventig een",
+	  "french": "soixante-onze",
+	  "italian": "settantuno"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 80,
+	  "dutch": "tachtig",
+	  "french": "quatre-vingts",
+	  "italian": "ottanta"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 81,
+	  "dutch": "tachtig een",
+	  "french": "quatre-vingts-un",
+	  "italian": "ottantuno"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 90,
+	  "dutch": "negentig",
+	  "french": "quatre-vingts-dix",
+	  "italian": "novanta"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 91,
+	  "dutch": "negentig een",
+	  "french": "quatre-vingts-onze",
+	  "italian": "novantuno"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 100,
+	  "dutch": "honderd",
+	  "french": "cent",
+	  "italian": "cento"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 200,
+	  "dutch": "tweehonderd",
+	  "french": "deux-cents",
+	  "italian": "duecento"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 300,
+	  "dutch": "driehonderd",
+	  "french": "trois-cents",
+	  "italian": "trecento"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 400,
+	  "dutch": "vierhonderd",
+	  "french": "quatre-cents",
+	  "italian": "quattrocento"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 500,
+	  "dutch": "vijfhonderd",
+	  "french": "cinq-cents",
+	  "italian": "cinquecento"
+	},
+	{
+	  "category": "Numbers",
+	  "english": 1000,
+	  "dutch": "duizend",
+	  "french": "mille",
+	  "italian": "mille"
+	}
+  ];
 	
